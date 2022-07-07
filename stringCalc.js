61f8e7037d4c9820afa98f36df5d1355d37622bd
sylvain <yvonneau.sylvain@gmail.com>
2022-07-07 14:57:11 +0200

    Step 2

--- stringCalc.js
+++ stringCalc.js
@@ -4,16 +4,18 @@ function strCalc(string) {
     if (string === '') {
         return 0;
     }
-    let a = string.split(',')[0];
-    let b = string.split(',')[1];
-    if (b == null) {
-        return parseInt(a);
-    } 
+    let listeStr = string.split(',');
+    let listeInt = listeStr.map(value => parseInt(value));
+
+    let somme =0;
+    listeInt.forEach(number => {
+        somme += number;
+    });
     
-    return parseInt(a)+parseInt(b);
+    return somme;
 }
 
 
-console.log(strCalc("10,2"));
+console.log(strCalc("10,2,5,3"));
 console.log(strCalc(""));
-console.log(strCalc("7"));
+console.log(strCalc("7,4"));
