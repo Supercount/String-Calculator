46ec517428d5e5cdf72fc30af55480a68d595479
sylvain <yvonneau.sylvain@gmail.com>
2022-07-07 14:52:41 +0200

    first commit

---
+++ stringCalc.js
@@ -0,0 +1,21 @@
+
+
+function strCalc(string) {
+    if (string === '') {
+        return 0;
+    }
+    let a = string.split(',')[0];
+    console.log("a : " + a);
+    let b = string.split(',')[1];
+    console.log("b : " + b);
+    if (b == null) {
+        return parseInt(a);
+    } 
+    
+    return parseInt(a)+parseInt(b);
+}
+
+
+console.log(strCalc("10,2"));
+console.log(strCalc(""));
+console.log(strCalc("7"));
